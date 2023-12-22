9aa7bf5e71f5a8ccdb73454687b12a6bbd4acf2f
Vitor Tenório de Lima <79546333+VitorTenor@users.noreply.github.com>
2023-12-22 10:11:36 -0300

    answers database delete

--- app/src/main/java/com/vitortenorio/descomplicando/infra/database/model/AnswerModel.java
+++
@@ -1,6 +0,0 @@
-package com.vitortenorio.descomplicando.infra.database.model;
-
-public record AnswerModel(
-        String id
-) {
-}
--- app/src/main/java/com/vitortenorio/descomplicando/infra/database/repository/AnswerDataRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vitortenorio.descomplicando.infra.database.repository;
-
-import com.vitortenorio.descomplicando.infra.database.storage.AnswerData;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnswerDataRepository extends AnswerData {
-}
