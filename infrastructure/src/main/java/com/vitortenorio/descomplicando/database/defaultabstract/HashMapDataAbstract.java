10e994de5e4f1f4efde7af31d6e3a18b8d34413e
Vitor Tenório de Lima <79546333+VitorTenor@users.noreply.github.com>
2023-12-23 13:38:05 -0300

    ajuste

--- infrastructure/src/main/java/com/vitortenorio/descomplicando/database/defaultabstract/HashMapDataAbstract.java
+++ infrastructure/src/main/java/com/vitortenorio/descomplicando/database/defaultabstract/HashMapDataAbstract.java
@@ -2,10 +2,7 @@ package com.vitortenorio.descomplicando.database.defaultabstract;
 
 import com.vitortenorio.descomplicando.database.repository.HashMapRepository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public abstract class HashMapDataAbstract<ID, T> implements HashMapRepository<ID, T> {
     protected final Map<ID, T> dataMap = new HashMap<>();
@@ -21,27 +18,7 @@ public abstract class HashMapDataAbstract<ID, T> implements HashMapRepository<ID
     }
 
     @Override
-    public T get(ID id) {
-        return dataMap.get(id);
-    }
-
-    @Override
-    public List<T> getAll() {
-        return new ArrayList<>(dataMap.values());
-    }
-
-    @Override
     public Map<ID, T> getAllMap() {
         return dataMap;
-    }
-
-    @Override
-    public void remove(ID id) {
-        dataMap.remove(id);
-    }
-
-    @Override
-    public void update(ID id, T data) {
-        dataMap.put(id, data);
     }
 }
--- infrastructure/src/main/java/com/vitortenorio/descomplicando/database/repository/impl/SingleQuestionRepositoryImpl.java
+++ infrastructure/src/main/java/com/vitortenorio/descomplicando/database/repository/impl/SingleQuestionRepositoryImpl.java
@@ -2,15 +2,13 @@ package com.vitortenorio.descomplicando.database.repository.impl;
 
 import com.vitortenorio.descomplicando.database.defaultabstract.HashMapDataAbstract;
 import com.vitortenorio.descomplicando.database.model.SingleQuestionModel;
-import com.vitortenorio.descomplicando.database.repository.HashMapRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public final class SingleQuestionRepositoryImpl extends HashMapDataAbstract<String, List<SingleQuestionModel>>
-        implements HashMapRepository<String, List<SingleQuestionModel>> {
+public final class SingleQuestionRepositoryImpl extends HashMapDataAbstract<String, List<SingleQuestionModel>> {
 
     @Override
     public void addOrCreate(String id, List<SingleQuestionModel> data) {
