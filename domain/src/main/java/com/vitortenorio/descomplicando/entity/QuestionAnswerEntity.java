925367c6d9a98c64ef01cb42620fc0923899d346
Vitor Tenório de Lima <79546333+VitorTenor@users.noreply.github.com>
2023-12-23 14:10:24 -0300

    ajuste

--- domain/src/main/java/com/vitortenorio/descomplicando/entity/QuestionAnswerEntity.java
+++ domain/src/main/java/com/vitortenorio/descomplicando/entity/QuestionAnswerEntity.java
@@ -1,34 +1,8 @@
 package com.vitortenorio.descomplicando.entity;
 
-public class QuestionAnswerEntity
-//        (
-//        String question,
-//        String answer,
-//        Integer answerId
-//)
-{
-    private String question;
-    private String answer;
-    private Integer answerId;
-
-    public QuestionAnswerEntity() {
-    }
-
-    public QuestionAnswerEntity(String question, String answer, Integer answerId) {
-        this.question = question;
-        this.answer = answer;
-        this.answerId = answerId;
-    }
-
-    public String question() {
-        return question;
-    }
-
-    public String answer() {
-        return answer;
-    }
-
-    public Integer answerId() {
-        return answerId;
-    }
+public record QuestionAnswerEntity(
+        String question,
+        String answer,
+        Integer answerId
+) {
 }
