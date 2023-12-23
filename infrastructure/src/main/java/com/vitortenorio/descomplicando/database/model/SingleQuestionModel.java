9f999a4581ed44bd02bec514597a0e304108778b
Vitor Tenório de Lima <79546333+VitorTenor@users.noreply.github.com>
2023-12-23 13:40:24 -0300

    ajuste

--- infrastructure/src/main/java/com/vitortenorio/descomplicando/database/model/SingleQuestionModel.java
+++ infrastructure/src/main/java/com/vitortenorio/descomplicando/database/model/SingleQuestionModel.java
@@ -1,9 +1,13 @@
 package com.vitortenorio.descomplicando.database.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public record SingleQuestionModel(
         String question,
         String answer,
+        @JsonIgnore
         Integer answerId,
+        @JsonIgnore
         String lesson
 ) {
 }
